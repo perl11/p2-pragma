fe976b858841686faa9dbeb5c0b5cf5ce44e8e73
Reini Urban <rurban@cpanel.net>
2013-03-04 23:05:50 -0600

    0.01 initial setup (must fail on p5p perl)

---
+++ p2.pm
@@ -0,0 +1,40 @@
+package p2;
+our $VERSION = '0.01';
+
+die "Sorry, p2 features not available for this perl $^X";
+
+1;
+__END__
+
+=head1 NAME
+
+p2 - activate p2 extensions
+
+=head1 SYNOPSIS
+
+  use p2;
+  # use extended p2 features and syntax, not found in p5p perl
+
+=head1 DESCRIPTION
+
+p2 is in planning stage still, and I don't believe p5p perl
+will be able to mimic those features. But who knows. A lot of
+perl6 and p5i was implemented.
+
+=head1 SEE ALSO
+
+L<http://perl11.org/p2/>
+
+=head1 AUTHOR
+
+Reini Urban, E<lt>rurban@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2013 by Reini Urban
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.14 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
